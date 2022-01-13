e2c6bf33ad2a6cdd25d15d7184146f0b521a0c5b
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 12:19:17 +0200

    Create Program.cs

---
+++ 01.First Steps In Coding - Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double cm = a * 2.54;
+            Console.WriteLine(cm);
+        }
+    }
+}
