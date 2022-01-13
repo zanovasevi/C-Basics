f26962fd6af84b17b68a69ccbba1423d6c85d9e4
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 12:20:19 +0200

    Create Program.cs

---
+++ 01.First Steps In Coding - Lab/06. Concatenate Data/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+            Console.WriteLine($"You are {firstName} {secondName}, " +
+                $"a {age}-years old person from {town}.");
+        }
+    }
+}
