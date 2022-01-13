6ed8f8fdcb4a1b5b18cbbb7fb514c42902984edf
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 12:18:35 +0200

    Create Program.cs

---
+++ 01.First Steps In Coding - Lab/03. Rectangle Area/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int area = a * a;
+            Console.WriteLine(area);
+        }
+    }
+}
