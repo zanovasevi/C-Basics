c3454d9a782f6f2f4e79dac2b9bd245a2580b547
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 12:18:07 +0200

    Create Program.cs

---
+++ 01.First Steps In Coding - Lab/02. Nums 1...10/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
