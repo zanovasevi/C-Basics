09b941864ccecc5e4ddea806a9db3f366d7eed4c
zanovasevi <70759869+zanovasevi@users.noreply.github.com>
2022-01-13 12:17:25 +0200

    Create Program.cs

---
+++ 01.First Steps In Coding - Lab/01. Hello SoftUni/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
